a11722abe6b9777ee559fe0ade03b3ae89e888a5
John Day <johntday@gmail.com>
2023-05-24 19:39:05 -0500

    original

--- modules/layout.py
+++ modules/layout.py
@@ -21,7 +21,8 @@ class Layout:
         st.markdown(
             """
             <div style='text-align: center;'>
-                <h4>Enter your <a href="https://platform.openai.com/account/api-keys" target="_blank">OpenAI API key</a> to start chatting  😉</h4>
+                <h4>Enter your <a href="https://platform.openai.com/account/api-keys" target="_blank">OpenAI API key</a> 
+                use the link to create a free API key.</h4>
             </div>
             """,
             unsafe_allow_html=True,
